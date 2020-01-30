b17afb2e1587fb1ef02603e63b3bf2d2ec03e81e
ImportMengjie <limengjie@hotmail.com>
2020-01-31 00:10:05 +0800

    add asyncio

---
+++ asyncio/spinner_asyncio.py
@@ -0,0 +1,47 @@
+import asyncio
+import itertools
+import time
+import sys
+
+
+class Signal:
+    go = True
+
+
+async def spin(msg):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle('|/-\\'):
+        status = char + ' ' + msg
+        write(status)
+        flush()
+        write('\x08' * len(status))
+        try:
+            await asyncio.sleep(.1)
+        except asyncio.CancelledError:
+            break
+    write(' ' * len(status) + '\x08' * len(status))
+
+
+async def slow_function():
+    await asyncio.sleep(1)
+    return 0
+
+
+async def supervisor():
+    spinner = asyncio.create_task(spin('thinking!'))
+    spinner.add_done_callback(lambda: print('haha'))
+    print('spinner object:', spinner)
+    result = await slow_function()
+    spinner.cancel()
+    return result
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(supervisor())
+    loop.close()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main()
     std::cout<<t.head()<<t.tail().head()<<t.tail().tail().head()<<std::endl;
     std::tuple<int,float,std::string> t2(1,1.1,"hello");
     std::get<2>(t2);
+    std::tuple_element<2, decltype(t2)>::type a;
 //    // test_poll();
 //    test_select2();
     return 0;
